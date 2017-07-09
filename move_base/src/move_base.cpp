5a0832e450c72ac87eda80ad2d401bec3107b698
Johannes Kummert <jkummert@techfak.uni-bielefeld.de>
2017-07-09 17:08:11 +0200

    reset plan on new goal

--- move_base/src/move_base.cpp
+++ move_base/src/move_base.cpp
@@ -639,6 +639,7 @@ namespace move_base {
 
     //we have a goal so start the planner
     boost::unique_lock<boost::recursive_mutex> lock(planner_mutex_);
+    planner_plan_->clear();
     planner_goal_ = goal;
     runPlanner_ = true;
     planner_cond_.notify_one();
